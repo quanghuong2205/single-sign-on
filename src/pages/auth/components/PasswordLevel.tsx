cc41465d5eafb91289bca51639c763298dcc1059
quanghuong2205 <quanghuong.dev@gmail.com>
2024-11-06 21:00:32 +0700

    Component: password strength

--- src/pages/auth/components/PasswordLevel.tsx
+++ src/pages/auth/components/PasswordLevel.tsx
@@ -1,9 +1,11 @@
 import { useMemo } from 'react';
-import passwordStyles from '../scss/password-level.module.scss';
-import { passwordStrength } from 'check-password-strength';
-import { MAX_PASSWORD_LEVEL, PASSWORD_LEVEL } from '../enum';
-import { generateArrayRange } from '@/utils';
 import classNames from 'classnames';
+import { passwordStrength } from 'check-password-strength';
+
+import passwordStyles from '../scss/password-level.module.scss';
+
+import { PASSWORD_LEVEL } from '../enum';
+import { generateArrayRange, isEmpty } from '@/utils';
 
 interface PasswordLevelProps {
   password: string;
@@ -11,7 +13,7 @@ interface PasswordLevelProps {
 
 const PasswordLevel: React.FC<PasswordLevelProps> = ({ password }) => {
   // flags
-  const isNotEmpty = password && password !== '';
+  const isNotEmpty = !isEmpty(password);
 
   // values
   const level = useMemo(() => {
@@ -19,9 +21,9 @@ const PasswordLevel: React.FC<PasswordLevelProps> = ({ password }) => {
 
     if (strength.length < PASSWORD_LEVEL.minLength) return 0;
 
-    const charTypeNum = strength.contains.filter((charType) => PASSWORD_LEVEL.characterTypes.includes(charType));
-    const matchedPercent = (charTypeNum.length * 100) / PASSWORD_LEVEL.characterTypes.length;
-    const level = Math.ceil((MAX_PASSWORD_LEVEL / 100) * matchedPercent);
+    const charTypes = strength.contains.filter((charType) => PASSWORD_LEVEL.characterTypes.includes(charType));
+    const matchedPercent = (charTypes.length * 100) / PASSWORD_LEVEL.characterTypes.length;
+    const level = Math.ceil(((PASSWORD_LEVEL.maxLevel + 1) / 100) * matchedPercent);
 
     return level - 1;
   }, [password]);
@@ -29,7 +31,7 @@ const PasswordLevel: React.FC<PasswordLevelProps> = ({ password }) => {
   return (
     <div className={passwordStyles.wrapper}>
       <div className={passwordStyles.levels}>
-        {generateArrayRange(0, MAX_PASSWORD_LEVEL + 1).map((v, i) => (
+        {generateArrayRange(0, PASSWORD_LEVEL.maxLevel + 1).map((v, i) => (
           <div
             key={`${v}${i}`}
             className={classNames({
