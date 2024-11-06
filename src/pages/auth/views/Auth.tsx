b774b1795cfcbabf666032fd0e2128f9512079c0
quanghuong2205 <quanghuong.dev@gmail.com>
2024-11-06 21:02:04 +0700

    Authpage

--- src/pages/auth/views/Auth.tsx
+++ src/pages/auth/views/Auth.tsx
@@ -1,36 +1,106 @@
-import { useState } from 'react';
-import authStyles from '../scss/auth.module.scss';
+import { useRef, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import classNames from 'classnames';
+
+import { ChevronLeftIcon, LoadingIcon, UserIcon } from '@/components';
 import AuthStrategies from '../components/AuthStrategies';
-import { ChevronLeftIcon, UserIcon } from '@/components';
 import LocalSignUpForm from './LocalSignupForm';
 import LocalSignInForm from './LocalSigninForm';
 import ForgotPasswordForm from './ForgotPasswordForm';
 import { Header } from '../layout';
-import { AUTH_TEXT } from '../enum';
+
+import authStyles from '../scss/auth.module.scss';
+import formStyles from '../scss/form.module.scss';
+
+import { AUTH_MODE_PARAM_KEY, AUTH_TEXT } from '../enum';
+import { AUTH_RESOLVERS } from '../models';
+
+const DEFAULT_AUTH_MODE: AuthModeType = 'SI';
 
 function Auth() {
+  // router
+  const [searchParams, setSearchParams] = useSearchParams({ mode: DEFAULT_AUTH_MODE });
+
+  // refs
+  const authModeStack = useRef<AuthModeType[]>([]);
+
   // states
-  const [authMode, setAuthMode] = useState<AuthModeType>('SU'); // SI = sign in, SU = sign up, FP = forgot password
-  const [isLocalAuth, setIsLocalAuth] = useState(true); // auth with email/phone number
+  const [authMode, setAuthMode] = useState<AuthModeType>(() => {
+    const authModeParam = searchParams.get(AUTH_MODE_PARAM_KEY) as AuthModeType;
+    const initialMode = authModeParam === 'RP' ? DEFAULT_AUTH_MODE : authModeParam;
+    authModeStack.current = [initialMode];
+    return initialMode;
+  }); // SI = sign in, SU = sign up, FP = forgot password, RP: reset password
+  const [isInLocalAuthMode, setIsInLocalAuthMode] = useState(false); // auth with email/phone number
+
+  // flags
+  const isShownBackBtn = authMode !== 'SI' || isInLocalAuthMode;
+  const isInForgotPasswordMode = authMode === 'RP' || authMode === 'FP';
+
+  // form validators
+  const {
+    handleSubmit: HandleSubmitHookForm,
+    formState: { errors, isValid, isSubmitting },
+    getValues,
+    reset: resetForm,
+    setValue,
+  } = useForm({
+    resolver: isInForgotPasswordMode ? AUTH_RESOLVERS['FP'] : AUTH_RESOLVERS[authMode],
+    mode: 'onChange',
+  });
 
   // constant values
   const authText = AUTH_TEXT[authMode];
-
-  // flags
-  const isShownBackBtn = authMode !== 'SI' || isLocalAuth;
+  const formValues = getValues();
 
   // handlers
-  const handleLocalAuthMode = () => setIsLocalAuth(!isLocalAuth);
-  const handleSwitchAuthMode = (mode: AuthModeType) => () => {
-    setAuthMode(mode);
-    handleLocalAuthMode();
+  const handleToggleLocalAuthMode = () => setIsInLocalAuthMode((prev) => !prev);
+  const handleApplyAuthModeToUrl = (mode: AuthModeType) => {
+    if (mode !== 'RP') {
+      setSearchParams({ mode });
+      resetForm({});
+    }
   };
-  const handleBackMode = () => handleSwitchAuthMode('SI')();
+
+  const handleSwitchAuthMode = (mode: AuthModeType) => () => {
+    if (mode !== 'RP') {
+      authModeStack.current.push(mode);
+    }
+    setAuthMode(mode);
+    isInLocalAuthMode && handleToggleLocalAuthMode();
+    handleApplyAuthModeToUrl(mode);
+  };
+
+  const handleBackAuthMode = () => {
+    authModeStack.current.pop();
+
+    const isEmptyStack = authModeStack.current.length === 0;
+    const currentMode = isEmptyStack ? DEFAULT_AUTH_MODE : authModeStack.current[authModeStack.current.length - 1];
+
+    setAuthMode(currentMode);
+    handleApplyAuthModeToUrl(currentMode);
+  };
+
+  const handleSetFieldValue = (name: string, value: string) => {
+    setValue(name, value, { shouldValidate: true });
+  };
+
+  const handleSubmit = async (data: any) => {
+    await new Promise((resolve) => setTimeout(() => resolve(data), 10000));
+    window.parent.postMessage(
+      {
+        type: 'LOGIN_SUCCESS',
+        message: 'HELLO',
+      },
+      '*',
+    );
+  };
 
   return (
     <div className={authStyles.wrapper}>
       {isShownBackBtn && (
-        <button className={authStyles.back} onClick={handleBackMode}>
+        <button className={authStyles.back} onClick={handleBackAuthMode}>
           <ChevronLeftIcon />
           <span>Quay lại</span>
         </button>
@@ -41,10 +111,10 @@ function Auth() {
 
         {/* Content */}
         <div className={authStyles.content}>
-          {!isLocalAuth && authMode !== 'FP' && (
+          {!isInLocalAuthMode && !isInForgotPasswordMode && (
             <>
               <AuthStrategies authMode={authText.mode} />
-              <button className={authStyles.strategy} onClick={handleLocalAuthMode}>
+              <button className={authStyles.strategy} onClick={handleToggleLocalAuthMode}>
                 <div className={authStyles['strategy-icon']}>
                   <UserIcon />
                 </div>
@@ -52,14 +122,53 @@ function Auth() {
               </button>
             </>
           )}
-          {isLocalAuth && authMode === 'SI' && <LocalSignInForm />}
-          {isLocalAuth && authMode === 'SU' && <LocalSignUpForm />}
-          {!isLocalAuth && authMode === 'FP' && <ForgotPasswordForm />}
+
+          {(isInLocalAuthMode || isInForgotPasswordMode) && (
+            <form onSubmit={(e: any) => e.preventDefault()}>
+              {isInLocalAuthMode && authMode === 'SI' && (
+                <LocalSignInForm onSetFieldValue={handleSetFieldValue} errors={errors} isSubmitting={isSubmitting} />
+              )}
+
+              {isInLocalAuthMode && authMode === 'SU' && (
+                <LocalSignUpForm
+                  onSetFieldValue={handleSetFieldValue}
+                  errors={errors}
+                  values={formValues}
+                  isSubmitting={isSubmitting}
+                />
+              )}
+
+              {isInForgotPasswordMode && (
+                <ForgotPasswordForm
+                  errors={errors}
+                  values={formValues}
+                  authMode={authMode}
+                  isSubmitting={isSubmitting}
+                  isValidForm={isValid}
+                  onSetFieldValue={handleSetFieldValue}
+                  onSubmit={HandleSubmitHookForm(handleSubmit)}
+                  onSetAuthMode={(mode: AuthModeType) => handleSwitchAuthMode(mode)()}
+                />
+              )}
+
+              {!isInForgotPasswordMode && (
+                <button
+                  disabled={!isValid || isSubmitting}
+                  onClick={HandleSubmitHookForm(handleSubmit)}
+                  className={classNames(formStyles.submit, {
+                    [formStyles['submit-disable']]: !isValid || isSubmitting,
+                  })}
+                >
+                  {!isSubmitting ? authText.submitTitle : <LoadingIcon cls="spinner" />}
+                </button>
+              )}
+            </form>
+          )}
         </div>
 
         {/* Footer */}
         <footer className={authStyles.footer}>
-          {authMode !== 'FP' && (
+          {!isInForgotPasswordMode && (
             <>
               <p className={authStyles.modes}>
                 <span>{authText.hint}</span>
--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -2,10 +2,9 @@ interface SignInByEmailRequest {
   email: string;
   password: string;
 }
-
 interface SignInByEmailResponse extends ApiResponse {}
 
-type AuthModeType = 'SI' | 'SU' | 'FP';
+type AuthModeType = 'SI' | 'SU' | 'FP' | 'RP';
 
 type AuthTextType = {
   title: string;
@@ -20,6 +19,27 @@ type AuthTextType = {
 type CharType = 'lowercase' | 'uppercase' | 'number' | 'symbol';
 type PasswordLevelType = {
   characterTypes: Array<CharType>;
+  regex: RegExp;
   minLength: number;
   label: { [key: number]: string };
+  maxLevel: number;
+};
+
+type SigninFieldValuesType = {
+  email: string;
+  password: string;
+};
+
+type SignupFieldValuesType = {
+  username: string;
+  email: string;
+  password: string;
+  otp: string;
+};
+
+type FortgotPasswordFieldValuesType = {
+  email: string;
+  otp: string;
+  password: string;
+  password_confirmation: string;
 };
